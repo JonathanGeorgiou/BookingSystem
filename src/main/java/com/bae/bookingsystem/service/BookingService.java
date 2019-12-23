d16c9b64aa4b41ce20b308dd464cd532eaa62c5f
JonathanGeorgiou <j.georgiou94@gmail.com>
2019-12-23 13:24:19 +0000

    updated booking service

--- src/main/java/com/bae/bookingsystem/service/BookingService.java
+++ src/main/java/com/bae/bookingsystem/service/BookingService.java
@@ -1,5 +1,7 @@
 package com.bae.bookingsystem.service;
 
+import java.util.List;
+
 import com.bae.bookingsystem.persistance.domain.Booking;
 import com.bae.bookingsystem.persistance.repo.BookingRepo;
 
@@ -19,6 +21,26 @@ public class BookingService {
 
     public Booking createBooking(Booking booking) {
         return bookingRepo.save(booking);
+    }
+
+    public Booking findBooking(Long id) {
+        return this.bookingRepo.findById(id).get();
+    }
+
+    public List<Booking> readBookings() {
+        return this.bookingRepo.findAll();
+    }
+
+    public Booking updateBooking(Booking booking, Long id) {
+        Booking bookingToUpdate = findBooking(id);
+        bookingToUpdate.setStyle(booking.getStyle());
+        bookingToUpdate.setTimeOfBooking(booking.getTimeOfBooking());
+        return this.bookingRepo.save(bookingToUpdate);
+    }
+
+    public String deleteBooking(Long id) {
+        this.bookingRepo.deleteById(id);
+        return "Booking succesfully deleted.";
     }
 
 }
