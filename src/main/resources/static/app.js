bda00a40ad80a8905d03d9e580faac7f5dba3ba0
JonathanGeorgiou <j.georgiou94@gmail.com>
2020-01-10 14:37:06 +0000

    able to save bookings to database

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,6 @@
-var hairstyle = "";
 function selectStyle() {
   const styleOptions = document.getElementsByName("optionsRadios");
+  let hairstyle = "";
 
   for (var i = 0; i < styleOptions.length; i++) {
     if (styleOptions[i].checked) {
@@ -11,20 +11,38 @@ function selectStyle() {
       //   timeOfBooking: "2012-08-23T18:25:43.511"
       // });
     }
-
+    alert("You selected " + hairstyle);
+    localStorage.setItem("style", hairstyle);
     location.assign("select-datetime.html");
-    return hairstyle;
   }
 }
 
 function selectTime() {
-  var hairstyle = selectStyle();
   var bookingTime = new Date($("#picker").val());
   alert(bookingTime);
-  console.log(hairstyle);
-  axios.patch("http://localhost:8081/updateBooking", {
+  localStorage.setItem("time", bookingTime);
+  location.assign("enter-details.html");
+}
+
+function confirmBooking() {
+  let bookingTime = localStorage.getItem("time");
+  let hairstyle = localStorage.getItem("style");
+
+  axios.post("http://localhost:8081/createBooking", {
     style: hairstyle,
     timeOfBooking: bookingTime
   });
-  //location.assign("enter-details.html");
+
+  axios.post("http://localhost:8081/createCustomer", {
+    firstName: "name",
+    lastName: "nameagain",
+    email: "emailaddress",
+    phoneNumber: "1291029109",
+    bookings: [
+      {
+        style: hairstyle,
+        timeOfBooking: bookingTime
+      }
+    ]
+  });
 }
