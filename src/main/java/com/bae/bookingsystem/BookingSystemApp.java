693b839aa52f5113a3d84c5d3373e4fa4f069430
JonathanGeorgiou <j.georgiou94@gmail.com>
2020-01-16 14:26:49 +0000

    fixing pom and main

--- src/main/java/com/bae/bookingsystem/BookingSystemApp.java
+++ src/main/java/com/bae/bookingsystem/BookingSystemApp.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookingSystemApp extends SpringBootServletInitializer {
+public class BookingSystemApp extends SpringBootServletInitializer
+
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingSystemApp.class, args);
