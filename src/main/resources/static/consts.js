c9dec5319531e00a80fe3b9c09b54c3c7b1cb26f
Ubuntu <ubuntu@ip-172-31-31-205.eu-west-2.compute.internal>
2020-01-22 16:40:51 +0000

    almost working

--- src/main/resources/static/consts.js
+++ src/main/resources/static/consts.js
@@ -1,2 +1,3 @@
-const PATH = "http://localhost:8081/";
-// const PATH = "/BookingSystem/";
+// const PATH = "http://localhost:8081/";
+const PATH = "/BookingSystem/";
+//const PATH = "http://localhost:8050/BookingSystem/";
