646b70cc0706fa929f342e30abfde84dd483185c
JonathanGeorgiou <58259275+JonathanGeorgiou@users.noreply.github.com>
2020-01-31 09:54:39 +0000

    Update consts.js

--- src/main/resources/static/consts.js
+++ src/main/resources/static/consts.js
@@ -2,4 +2,4 @@
 // const PATH = "/BookingSystem/";
 // const PATH = "http://3.11.74.92:8050/BookingSystem/";
 // const PATH = "http://localhost:8050/BookingSystem/";
-const PATH = "http://back-end-load-balancer-676731506.eu-west-2.elb.amazonaws.com:8080/BookingSystem/"
+const PATH = "http://back-end-load-balancer-1499800575.eu-west-2.elb.amazonaws.com:8080/BookingSystem/"
